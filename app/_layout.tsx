0d3a2bd482f4cfa33fdba39ee5a7514d8270c10c
shuvo-me <shuvo.js.dev@gmail.com>
2025-10-12 23:00:16 +0600

    setup: colors & fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,25 @@
 import "@/assets/css/global.css";
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import { useEffect } from "react";
+
+// SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [fontsLoaded, error] = useFonts({
+    "Rubik-Bold": require("../assets/fonts/Rubik-Bold.ttf"),
+    "Rubik-ExtraBold": require("../assets/fonts/Rubik-ExtraBold.ttf"),
+    "Rubik-Light": require("../assets/fonts/Rubik-Light.ttf"),
+    "Rubik-Medium": require("../assets/fonts/Rubik-Medium.ttf"),
+    "Rubik-Regular": require("../assets/fonts/Rubik-Regular.ttf"),
+    "Rubik-Semibold": require("../assets/fonts/Rubik-Semibold.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded && !error?.message) {
+      // SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
   return (
     <Stack
       screenOptions={{
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,28 @@ module.exports = {
   content: ["./App.tsx","./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}" ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        rubik: ['Rubik-Regular','sans-serif'],
+         'rubik-bold': ['Rubik-Bold', 'sans-serif'],
+          'rubik-extrabold': ['Rubik-ExtraBold', 'sans-serif'],
+          'rubik-light': ['Rubik-Light', 'sans-serif'],
+          'rubik-semibold': ['Rubik-SemiBold', 'sans-serif'],
+          'rubik-medium': ['Rubik-Medium', 'sans-serif'],
+      },
+      colors:{
+        primary: '#FF8000',
+        black:{
+          1: '#191D31',
+          2: '#666876',
+          3: '#8C8E98'
+        },
+        accent: {
+          100: '#FBFBFD'
+        },
+        danger: '#F75555'
+      }
+    },
   },
   plugins: [],
 }
