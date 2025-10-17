dd08902a6a01f0e728a2ec08a8b9dfedf161ef7b
shuvo-me <shuvo.js.dev@gmail.com>
2025-10-17 11:59:46 +0600

    feat: added ActivityIndicator

--- app/(protected)/_layout.tsx
+++ app/(protected)/_layout.tsx
@@ -1,8 +1,20 @@
 import { useAuthStore } from "@/lib/store";
 import { Redirect, Stack } from "expo-router";
+import { ActivityIndicator } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const ProtectedAppLayout = () => {
   const session = useAuthStore((state) => state.session);
+
+  const hasRehydratedAuth = useAuthStore((state) => state._isHydrated);
+
+  if (!hasRehydratedAuth) {
+    return (
+      <SafeAreaView className=" flex-1 justify-center">
+        <ActivityIndicator className=" text-primary" />
+      </SafeAreaView>
+    );
+  }
 
   if (!session) {
     return <Redirect href={"/signin"} />;
