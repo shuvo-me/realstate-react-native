c7036e3914162ab87a418588140f9010aeba80b8
shuvo-me <shuvo.js.dev@gmail.com>
2025-10-18 11:47:13 +0600

    setup: tanstack react query

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,12 @@
 import "@/assets/css/global.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 
 SplashScreen.preventAutoHideAsync();
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
   const [fontsLoaded, error] = useFonts({
@@ -27,12 +29,14 @@ export default function RootLayout() {
   }
 
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-      }}
-    >
-      <Stack.Screen name="(protected)" />
-    </Stack>
+    <QueryClientProvider client={queryClient}>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="(protected)" />
+      </Stack>
+    </QueryClientProvider>
   );
 }
