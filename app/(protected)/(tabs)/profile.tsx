6288047a7802a81fb8dc03b1b7b95028bd3b2726
shuvo-me <shuvo.js.dev@gmail.com>
2025-10-18 10:20:29 +0600

    design: profile header

--- app/(protected)/(tabs)/profile.tsx
+++ app/(protected)/(tabs)/profile.tsx
@@ -1,11 +1,21 @@
+import icons from "@/constants/icons";
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const ProfileScreen = () => {
   return (
-    <View>
-      <Text>ProfileScreen</Text>
-    </View>
+    <SafeAreaView className="h-full">
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerClassName="pb-32 px-7"
+      >
+        <View className=" mt-5 flex-row items-center justify-between">
+          <Text className=" font-rubik-semibold text-xl">Profile</Text>
+          <Image source={icons.bell} className=" size-5" resizeMode="contain" />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
