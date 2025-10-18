e4051635f13fdf8a35ab8ef3e60608ec9e3173af
shuvo-me <shuvo.js.dev@gmail.com>
2025-10-18 10:29:16 +0600

    design: profile image and name

--- app/(protected)/(tabs)/profile.tsx
+++ app/(protected)/(tabs)/profile.tsx
@@ -1,6 +1,7 @@
 import icons from "@/constants/icons";
+import images from "@/constants/images";
 import React from "react";
-import { Image, ScrollView, Text, View } from "react-native";
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const ProfileScreen = () => {
@@ -13,6 +14,25 @@ const ProfileScreen = () => {
         <View className=" mt-5 flex-row items-center justify-between">
           <Text className=" font-rubik-semibold text-xl">Profile</Text>
           <Image source={icons.bell} className=" size-5" resizeMode="contain" />
+        </View>
+        <View className="items-center mt-6 gap-4">
+          <View className=" relative">
+            <Image
+              source={images.avatar}
+              className=" size-44 rounded-full"
+              resizeMode="contain"
+            />
+            <TouchableOpacity className=" absolute right-3 bottom-0">
+              <Image
+                source={icons.edit}
+                tintColor={"#FF8000"}
+                className=" size-9"
+              />
+            </TouchableOpacity>
+          </View>
+          <Text className=" font-rubik-semibold text-2xl text-black-1">
+            ShuvoJsDev
+          </Text>
         </View>
       </ScrollView>
     </SafeAreaView>
